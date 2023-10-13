3e1234f8d9bcc39fb906f23e2649f593e0c3627f
Laurentiu Calin Durnea <durnea.calin@gmail.com>
2023-10-13 19:10:03 +0200

    Update Startup.cs

--- backend/BillSplit.Api/Startup.cs
+++ backend/BillSplit.Api/Startup.cs
@@ -48,8 +48,8 @@ public class Startup
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-        if (env.IsDevelopment())
-        {
+        // if (env.IsDevelopment())
+        // {
             app.UseSwagger();
             app.UseSwaggerUI(options =>
             {
@@ -61,13 +61,13 @@ public class Startup
 
             app.UseDeveloperExceptionPage();
             app.SeedData();
-        }
-        else
-        {
-            app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
+        // }
+        // else
+        // {
+        //     app.UseExceptionHandler("/Error");
+        //     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        //     app.UseHsts();
+        // }
 
         app.UseExceptionHandler(options => { });
         app.UseHttpsRedirection();
