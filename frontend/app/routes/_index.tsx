097f1ee5f45ec95ac5a39cfc4c9626a5e0177158
Gonzalo Stoll <gonzalostoll@gonzalo.local>
2023-11-27 19:15:45 +0100

    Initial frontend boilerplate

---
+++ frontend/app/routes/_index.tsx
@@ -0,0 +1,14 @@
+export function meta() {
+  return [
+    {title: 'BillSplit'},
+    {name: 'description', content: 'Welcome BillSplit'},
+  ]
+}
+
+export default function Index() {
+  return (
+    <div>
+      <h1>Hello world!</h1>
+    </div>
+  )
+}
