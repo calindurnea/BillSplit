a94ba1e151bc82c2a6d77c35a5b5fe05de604c0e
Gonzalo Stoll <stollgonzalo@gmail.com>
2024-01-21 00:01:21 +0100

    Undo index changes

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -1,20 +1,3 @@
-import type {LoaderFunctionArgs} from '@remix-run/node'
-import {authFetch} from '~/utils/auth-fetch'
-import {authenticate} from '~/utils/session.server'
-
-export async function loader({request}: LoaderFunctionArgs) {
-  const token = await authenticate(request)
-  try {
-    const response = await authFetch(token, 'http://localhost:5003/api/Users')
-    const data = await response.json()
-    console.log('logging data: ', data)
-  } catch (error) {
-    console.error(error)
-    throw error
-  }
-  return null
-}
-
 export default function Index() {
   return (
     <div>
